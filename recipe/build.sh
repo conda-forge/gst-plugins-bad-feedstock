098f1a22cf8519e31429545df475744e76d54ced
Mark Harfouche <mark.harfouche@gmail.com>
2023-05-21 14:33:22 -0400

    ONly for corss

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,11 @@ pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-    echo "objcpp = '${CXX}'" >> ${BUILD_PREFIX}/meson_cross_file.txt
-    cat ${BUILD_PREFIX}/meson_cross_file.txt
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+    if [[ "${target_platform}" == "osx-arm64" ]]; then
+        echo "objcpp = '${CXX}'" >> ${BUILD_PREFIX}/meson_cross_file.txt
+        cat ${BUILD_PREFIX}/meson_cross_file.txt
+    fi
 fi
 
 meson_options=(
