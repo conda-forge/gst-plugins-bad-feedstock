7121ad2ee9ad3f190ebc2442ef4589f3846eb381
Mark Harfouche <mark.harfouche@gmail.com>
2023-05-21 14:29:34 -0400

    Try to specify the cross

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,11 @@ pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    echo "objcpp = '${CXX}'" >> ${BUILD_PREFIX}/meson_cross_file.txt
+    cat ${BUILD_PREFIX}/meson_cross_file.txt
+fi
+
 meson_options=(
       -Dexamples=disabled
       -Dtests=disabled
