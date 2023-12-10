0b6f0e35cc606e14df8335f471f10c0bb130b970
Mark Harfouche <mark.harfouche@gmail.com>
2023-12-10 09:01:44 -0500

    Use environment variable instead of editing cross meson thank @isuruf

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,9 @@ pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
-    if [[ "${target_platform}" == "osx-arm64" ]]; then
-        echo "objcpp = '${CXX}'" >> ${BUILD_PREFIX}/meson_cross_file.txt
-        cat ${BUILD_PREFIX}/meson_cross_file.txt
-    fi
+if [[ "${target_platform}" == "osx-"* ]]; then
+    export OBJCXX=${CXX}
+    export OBJCXX_FOR_BUILD=${CXX_FOR_BUILD}
 fi
 
 meson_options=(
